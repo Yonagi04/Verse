1f5f9ca9d780a7c2862491d797568f292e9b02b2
Yonagi <1809266700@qq.com>
2026-05-18 19:08:58 +0800

    create verse

---
+++ src/main/java/com/yonagi/verse/VerseApplication.java
@@ -0,0 +1,14 @@
+package com.yonagi.verse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class VerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerseApplication.class, args);
+    }
+}
